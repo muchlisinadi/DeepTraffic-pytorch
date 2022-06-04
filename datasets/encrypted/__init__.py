c1125750523674a4672cc9edd26a4c6e56f181b0
muchlisinadi <muchlisinadi@gmail.com>
2022-06-04 11:44:13 +0700

    add encrypted dataset

---
+++ datasets/encrypted/__init__.py
@@ -0,0 +1,16 @@
+from .encrypted_2class import (Encrypted2ClassFlowAllLayers,
+                               Encrypted2ClassFlowL7,
+                               Encrypted2ClassSessionAllLayers,
+                               Encrypted2ClassSessionL7)
+from .encrypted_6class import (Encrypted6ClassNovpnFlowAllLayers,
+                               Encrypted6ClassNovpnFlowL7,
+                               Encrypted6ClassNovpnSessionAllLayers,
+                               Encrypted6ClassNovpnSessionL7,
+                               Encrypted6ClassVpnFlowAllLayers,
+                               Encrypted6ClassVpnFlowL7,
+                               Encrypted6ClassVpnSessionAllLayers,
+                               Encrypted6ClassVpnSessionL7)
+from .encrypted_12class import (Encrypted12ClassFlowAllLayers,
+                                Encrypted12ClassFlowL7,
+                                Encrypted12ClassSessionAllLayers,
+                                Encrypted12ClassSessionL7)
